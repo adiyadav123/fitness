03ec656d33a8771a00c9f3cef955ccea969b913d
Aditya Yadav <ramanandyadav7650@gmail.com>
2023-10-07 22:33:28 +0530

    update

--- lib/view/profile/profile_view.dart
+++ lib/view/profile/profile_view.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fit/common/color_extension.dart';
+import 'package:fit/common_widget/round_button.dart';
+import 'package:fit/view/home/home_view.dart';
+import 'package:fit/view/home/online_home.dart';
 import 'package:fit/view/login/sign_up_view.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -12,9 +17,46 @@ class ProfileView extends StatefulWidget {
 
 class _ProfileViewState extends State<ProfileView> {
   var auth = FirebaseAuth.instance;
+  var gender = "Male";
+  var image = "images/user.png";
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    var auth = FirebaseAuth.instance;
+    var user = auth.currentUser?.displayName;
+    final FirebaseFirestore firestore = FirebaseFirestore.instance;
+    final DocumentSnapshot snapshot =
+        await firestore.collection('users').doc(user).get();
+
+    if (snapshot.exists) {
+      // Data exists, you can access it using snapshot.data()
+      gender = snapshot['gender'];
+
+      if (gender == 'male' || gender == "Male") {
+        if (!mounted) return;
+        setState(() {
+          image = "images/male_profile_two.png";
+        });
+      } else if (gender == "female" || gender == "Female") {
+        if (!mounted) return;
+        setState(() {
+          image = "images/user.png";
+        });
+      }
+    } else {
+      // Document doesn't exist
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    var name = auth.currentUser?.displayName;
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: SingleChildScrollView(
@@ -30,7 +72,10 @@ class _ProfileViewState extends State<ProfileView> {
                     children: [
                       IconButton(
                         onPressed: () {
-                          Navigator.pop(context);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => OnlineHomePageView()));
                         },
                         icon: Image.asset("images/before_nav.png"),
                       ),
@@ -48,6 +93,45 @@ class _ProfileViewState extends State<ProfileView> {
                       ),
                     ],
                   ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Transform.scale(
+                        scale: 1.3,
+                        child: Image.asset(
+                          image,
+                          height: 60,
+                          width: 60,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          name!,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "Lose fat program",
+                          style: TextStyle(
+                            color: TColor.darkgray,
+                            fontFamily: "Poppins",
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
                 )
               ],
             ),
